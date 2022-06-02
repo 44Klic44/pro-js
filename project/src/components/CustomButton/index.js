bd8c37e1cb1b86168f4ceacb40425936b7e3815d
ROMAN <stance_srt@mail.ru>
2022-06-02 20:51:30 +1000

    delit lesson 7

--- project/src/components/CustomButton/index.js
+++
@@ -1,7 +0,0 @@
-export default Vue.component('custom-button', {
-  template: `
-      <button class="search-button custom-button" type="button" v-on:click="$emit('click')">
-         <slot></slot>
-      </button>
-    `
-})
